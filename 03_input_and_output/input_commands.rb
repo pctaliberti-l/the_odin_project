9868ab68dae8c04a310e61c11bd3feb6fc729198
P. C. Taliberti L <pauloct-lovo@live.com>
2020-08-19 15:15:48 -0300

    output and input lessons from TOP

---
+++ 03_input_and_output/input_commands.rb
@@ -0,0 +1,13 @@
+# Input Commands
+
+## To accept input from a user, we can use the gets command.
+gets
+
+## the program execution will stop and wait for user input. After the user presses Enter, the program will continue its execution.
+## Unlike puts and print, gets actually returns the user input instead of returning nil.
+
+### The gets command always returns a new line at the end of the input.
+### .chomp is a method commonly used to trim separators
+
+gets.chomp
+
---
+++ 03_input_and_output/output_commands.rb
@@ -0,0 +1,19 @@
+# Output Commands: print & puts
+
+## puts and print commands are quite similar
+### puts appends a new line to the argument passed in
+puts "This is a puts command."
+### print keeps thins all on one line
+print "This is a print command."
+
+### in irb mode both commands after printing whatever argument they are passed
+### both return nil
+
+## To highlight the difference between puts and print, we're going to use a semicolon.
+### they allow you to run multiple commands all on one line.
+
+# puts
+puts "This is a puts command."; puts "This is other puts command."
+
+# print
+print "This is a print command."; print "This is other print command."
